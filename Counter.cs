9af0eed9c022bd60ae3bb7c2a9f7d095efcdc2bb
MylesSHarris <69069959+MylesSHarris@users.noreply.github.com>
2020-12-21 13:56:48 -0800

    Add files via upload

---
+++ Counter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace Homework
+{
+    public class Counter
+    {
+        private int count;
+
+        //Include a getter method that returns the current count value and a method that outputs the count to the screen.
+        public void ResetCounter()
+        {
+            count = 0;
+        }
+
+        public void IncreaseCount()
+        {
+            count++;
+        }
+
+        public void DecreaseCount()
+        {
+            if (count > 0) count--;
+        }
+
+        public int GetCount()
+        {
+            return count;
+        }
+
+        public void PrintCount()
+        {
+            Console.WriteLine(count);
+        }
+
+        public override string ToString()
+        {
+            return count.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+    }
+}
